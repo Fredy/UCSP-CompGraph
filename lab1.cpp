166aabcd78d6a6a8862fe792003ef407fd7a05b7
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-08-28 01:43:30 -0500

    Circle draw improved and all functions work properly, anti-aliasing added

--- lab1.cpp
+++ lab1.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 const GLuint WIDTH = 800, HEIGHT = 600;
-const int CIRCLE_VERTEX_AMOUNT = 600;
+const int CIRCLE_VERTEX_AMOUNT = 200;
 constexpr const float PI = 3.141592653589793f;
 
 struct DataWrapper {
@@ -55,10 +55,9 @@ DataWrapper createData(GLsizei size, const GLvoid *data) {
   glBindVertexArray(0);
 
   return {vao, vbo}; 
-
 }
 
-DataWrapper createSquare(array<float, 2> center, float edge) {
+vector<DataWrapper> createSquare(array<float, 2> center, float edge) {
   float bottomLeft[] = {center[0] - edge / 2, center[1] - edge / 2};
   GLfloat vertices[] = {
     bottomLeft[0], bottomLeft[1], 0.0f,
@@ -68,57 +67,45 @@ DataWrapper createSquare(array<float, 2> center, float edge) {
   };
 
   DataWrapper data = createData(sizeof(vertices), vertices);
-  return data; 
+  return {data}; 
 }
 
 vector<float> genCircleVertices(const array<float, 2> center, const float radius, const int vertexAmount) {
   vector<float> vertices(vertexAmount * 3);
 
-  const float kSq = center[1] * center[1]; 
-  const float rSq = radius * radius;
-  const float stride = (radius * 2 / vertexAmount) * 2;
-  const float h0 = center[0] - radius;
-
-  for (int i = 0; i < vertexAmount / 2; i++) {
-    float x = h0 + stride * i;
-    float tmp = center[0] - x;
-    float sqrtR = sqrt(4 * kSq - 4 * (kSq +  tmp * tmp - rSq)) / 2; 
-    float yTop = center[1] + sqrtR;
-    float yBot = center[1] - sqrtR;
-    vertices[i * 3] = x;
-    vertices[i * 3 + 1] = yTop;
+  for (int i = 0; i < vertexAmount; i++) {
+    vertices[i * 3] = center[0] + radius * cos((i + 1.0f) * 2.0f * PI / float(vertexAmount));
+    vertices[i * 3 + 1] = center[1] + radius * sin((i + 1.0f) * 2.0f * PI / float(vertexAmount));
     vertices[i * 3 + 2] = 0.0f;
-    // Setting the mirror vertex.
-    *(vertices.rbegin() + i * 3 + 2) = x;
-    *(vertices.rbegin() + i * 3 + 1) = yBot;
-    *(vertices.rbegin() + i * 3) = 0.0f;
   }
   return  vertices;
 }
 
-DataWrapper createCircle(const array<float, 2> center, const float radius) {
+vector<DataWrapper> createCircle(const array<float, 2> center, const float radius) {
   vector<GLfloat> vertices = genCircleVertices(center, radius, CIRCLE_VERTEX_AMOUNT);
 
   DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
-  return data;
+  return {data};
 }
 
 // - center and radius ar the center and radius of the biggest circle,
 // - reduction is the ratio that the radius of a circle will have compared to 
 //   the radius of the previous circle, 
 // - amount is the number of circles that will be drawn
-DataWrapper createCirclesInsideCircle(const array<float, 2> center, float radius, const float reduction, const int amount) {
+vector<DataWrapper> createCirclesInsideCircle(const array<float, 2> center, float radius, const float reduction, const int amount) {
+  vector<DataWrapper> data(amount);
   vector<GLfloat> vertices = genCircleVertices(center, radius, CIRCLE_VERTEX_AMOUNT);
 
+  data[0] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
+  
   const float x0 = center[0] - radius;
   for (int i = 1; i < amount; i++) {
     radius = radius * reduction;
     const float tmpCenter = x0 + radius; 
-    vector<GLfloat> tmp = genCircleVertices({tmpCenter, center[1]}, radius, CIRCLE_VERTEX_AMOUNT);
-    vertices.insert(vertices.end(), tmp.begin(), tmp.end());
+    vertices = genCircleVertices({tmpCenter, center[1]}, radius, CIRCLE_VERTEX_AMOUNT);
+    data[i] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
   }
 
-  DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
   return data;
 }
 
@@ -126,8 +113,10 @@ DataWrapper createCirclesInsideCircle(const array<float, 2> center, float radius
 // - reduction is the ratio that the radius of a circle will have compared to 
 //   the radius of the previous circle, 
 // - amount is the number of circles that will be drawn
-DataWrapper createCirclesLine1(const array<float, 2> center, float radius, const float reduction, const int amount) {
+vector<DataWrapper> createCirclesLine1(const array<float, 2> center, float radius, const float reduction, const int amount) {
+  vector<DataWrapper> data(amount);
   vector<GLfloat> vertices = genCircleVertices(center, radius, CIRCLE_VERTEX_AMOUNT);
+  data[0] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
 
   float lastX = center[0] + radius;
 
@@ -135,11 +124,10 @@ DataWrapper createCirclesLine1(const array<float, 2> center, float radius, const
     radius = radius * reduction;
     const float tmpCenter = lastX + radius; 
     lastX = tmpCenter + radius;
-    vector<GLfloat> tmp = genCircleVertices({tmpCenter, center[1]}, radius, CIRCLE_VERTEX_AMOUNT);
-    vertices.insert(vertices.begin() + vertices.size() / 2, tmp.begin(), tmp.end());
+    vertices = genCircleVertices({tmpCenter, center[1]}, radius, CIRCLE_VERTEX_AMOUNT);
+    data[i] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
   }
 
-  DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]);
   return data;
 }
 
@@ -152,8 +140,10 @@ constexpr float degToRad(float angle) {
 // - reduction is the ratio that the radius of a circle will have compared to 
 //   the radius of the previous circle, 
 // - amount is the number of circles that will be drawn
-DataWrapper createCirclesLine2(const array<float, 2> center, float radius, const float angle, const float reduction, const int amount) {
+vector<DataWrapper> createCirclesLine2(const array<float, 2> center, float radius, const float angle, const float reduction, const int amount) {
+  vector<DataWrapper> data(amount);
   vector<GLfloat> vertices = genCircleVertices(center, radius, CIRCLE_VERTEX_AMOUNT);
+  data[0] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
 
   float hypotenuse = radius; // This will be the distance between the first center and the current center.
 
@@ -164,30 +154,13 @@ DataWrapper createCirclesLine2(const array<float, 2> center, float radius, const
       center[0] + cos(degToRad(angle)) *  hypotenuse,
       center[1] + sin(degToRad(angle)) *  hypotenuse,
     };
-    vector<GLfloat> tmp = genCircleVertices(currentCenter, radius, CIRCLE_VERTEX_AMOUNT);
-    //vertices.insert(vertices.begin() + vertices.size() / 2, tmp.begin(), tmp.end());
-    vertices.insert(vertices.end(), tmp.begin(), tmp.end());
+    vertices = genCircleVertices(currentCenter, radius, CIRCLE_VERTEX_AMOUNT);
+    data[i] = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
 
     hypotenuse += radius;
   }
 
-  GLuint vbo, vao;
-  glGenVertexArrays(1, &vao);
-  glGenBuffers(1, &vbo);
-
-  glBindVertexArray(vao);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
-  glEnableVertexAttribArray(0);
-
-  // Unbinding the buffer because it is already registered in the VAO.
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  // This is optional, unbinding VAO.
-  glBindVertexArray(0);
-
-  return {vao, vbo}; 
+  return data;
 }
 
 
@@ -260,30 +233,37 @@ int main() {
   }
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
+  vector<DataWrapper> dataVec ;
+  // dataVec = createSquare({-100.0f, -100.0f}, 200.0f); // 1 square
+  // dataVec = createCircle({100.0f, -100.0f}, 400.0); // 2 circle
+  // dataVec = createCirclesInsideCircle({0.0f, 100.0f}, 400.0f, 0.8f, 6); // 3 circles inside circle
+  // dataVec = createCirclesLine1({-500.0f, 0.0f}, 200.0f, 0.8f, 6); // 4 circle line
+  // dataVec = createCirclesLine2({-500.0f, 0.0f}, 150.0f, 25, 0.8f, 6); // 5 circle line with inclination
 
-  DataWrapper square = createSquare({-100.0f, -100.0f}, 200.0f); // 1
-  DataWrapper circle = createCircle({-100.0f, -100.0f}, 400.0); // 2
-  const int circlesAmount = 6;
-  DataWrapper circlesIn = createCirclesInsideCircle({0.0f, 100.0f}, 400.0f, 0.8f, circlesAmount); // 3
-  DataWrapper circlesLine1 = createCirclesLine1({-500.0f, 0.0f}, 200.0f, 0.8f, circlesAmount); // 4
-  DataWrapper circlesLine2 = createCirclesLine2({-500.0f, 0.0f}, 150.0f, 25, 0.8f, circlesAmount); // 5
+  // Anti aliasing
+  glEnable(GL_LINE_SMOOTH);
+  glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Main loop
+  // Main loop
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.95f, 0.95f, 0.95f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
-    glBindVertexArray(circlesLine1.vao);
-    glDrawArrays(GL_LINE_LOOP, 0, CIRCLE_VERTEX_AMOUNT * circlesAmount);
-
+    for (const auto &data : dataVec) {
+      glBindVertexArray(data.vao);
+      glDrawArrays(GL_LINE_LOOP, 0, CIRCLE_VERTEX_AMOUNT); // TODO: change this so it works with the square too
+    }
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  // TODO: delete VAOs and VBOs;
-  glDeleteVertexArrays(1, &square.vao);
-  // glDeleteBuffers(1, &vbo);
+  for (const auto &data : dataVec) {
+    glDeleteVertexArrays(1, &data.vao);
+    glDeleteBuffers(1, &data.vbo);
+  }
 
   glfwTerminate();
 }
