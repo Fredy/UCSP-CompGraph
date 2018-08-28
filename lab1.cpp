ef6e278eff94d0cc9f0b19e15cf3cecb4fb72d53
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-08-28 00:53:27 -0500

    Refactoring creation of VAO and VBO

--- lab1.cpp
+++ lab1.cpp
@@ -37,22 +37,14 @@ void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }  
 
-DataWrapper createSquare(array<float, 2> center, float edge) {
-  float bottomLeft[] = {center[0] - edge / 2, center[1] - edge / 2};
-  GLfloat vertices[] = {
-    bottomLeft[0], bottomLeft[1], 0.0f,
-    bottomLeft[0], bottomLeft[1] + edge, 0.0f,
-    bottomLeft[0] + edge, bottomLeft[1] + edge, 0.0f,
-    bottomLeft[0] + edge, bottomLeft[1], 0.0f,
-  };
-
+DataWrapper createData(GLsizei size, const GLvoid *data) {
   GLuint vbo, vao;
   glGenVertexArrays(1, &vao);
   glGenBuffers(1, &vbo);
 
   glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
   glEnableVertexAttribArray(0);
@@ -63,13 +55,23 @@ DataWrapper createSquare(array<float, 2> center, float edge) {
   glBindVertexArray(0);
 
   return {vao, vbo}; 
+
+}
+
+DataWrapper createSquare(array<float, 2> center, float edge) {
+  float bottomLeft[] = {center[0] - edge / 2, center[1] - edge / 2};
+  GLfloat vertices[] = {
+    bottomLeft[0], bottomLeft[1], 0.0f,
+    bottomLeft[0], bottomLeft[1] + edge, 0.0f,
+    bottomLeft[0] + edge, bottomLeft[1] + edge, 0.0f,
+    bottomLeft[0] + edge, bottomLeft[1], 0.0f,
+  };
+
+  DataWrapper data = createData(sizeof(vertices), vertices);
+  return data; 
 }
 
 vector<float> genCircleVertices(const array<float, 2> center, const float radius, const int vertexAmount) {
-  // center = (h, k)
-  // r² = (h - x)² + (k - y)²
-  // y = (2 * k ± sqrt((2 * k)² - 4 * (k² + (h - x)² - r²))) / 2
-
   vector<float> vertices(vertexAmount * 3);
 
   const float kSq = center[1] * center[1]; 
@@ -97,23 +99,8 @@ vector<float> genCircleVertices(const array<float, 2> center, const float radius
 DataWrapper createCircle(const array<float, 2> center, const float radius) {
   vector<GLfloat> vertices = genCircleVertices(center, radius, CIRCLE_VERTEX_AMOUNT);
 
-  GLuint vbo, vao;
-  glGenVertexArrays(1, &vao);
-  glGenBuffers(1, &vbo);
-
-  glBindVertexArray(vao);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
-  glEnableVertexAttribArray(0);
-
-  // Unbinding the buffer because it is already registered in the VAO.
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  // This is optional, unbinding VAO.
-  glBindVertexArray(0);
-
-  return {vao, vbo}; 
+  DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
+  return data;
 }
 
 // - center and radius ar the center and radius of the biggest circle,
@@ -131,23 +118,8 @@ DataWrapper createCirclesInsideCircle(const array<float, 2> center, float radius
     vertices.insert(vertices.end(), tmp.begin(), tmp.end());
   }
 
-  GLuint vbo, vao;
-  glGenVertexArrays(1, &vao);
-  glGenBuffers(1, &vbo);
-
-  glBindVertexArray(vao);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
-  glEnableVertexAttribArray(0);
-
-  // Unbinding the buffer because it is already registered in the VAO.
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  // This is optional, unbinding VAO.
-  glBindVertexArray(0);
-
-  return {vao, vbo}; 
+  DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]) ;
+  return data;
 }
 
 // - center and radius ar the center and radius of the biggest circle,
@@ -167,23 +139,8 @@ DataWrapper createCirclesLine1(const array<float, 2> center, float radius, const
     vertices.insert(vertices.begin() + vertices.size() / 2, tmp.begin(), tmp.end());
   }
 
-  GLuint vbo, vao;
-  glGenVertexArrays(1, &vao);
-  glGenBuffers(1, &vbo);
-
-  glBindVertexArray(vao);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
-  glEnableVertexAttribArray(0);
-
-  // Unbinding the buffer because it is already registered in the VAO.
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  // This is optional, unbinding VAO.
-  glBindVertexArray(0);
-
-  return {vao, vbo}; 
+  DataWrapper data = createData(sizeof(GLfloat) * vertices.size(), &vertices[0]);
+  return data;
 }
 
 constexpr float degToRad(float angle) {
@@ -317,7 +274,7 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
-    glBindVertexArray(circlesLine2.vao);
+    glBindVertexArray(circlesLine1.vao);
     glDrawArrays(GL_LINE_LOOP, 0, CIRCLE_VERTEX_AMOUNT * circlesAmount);
 
     glfwSwapBuffers(window);
